d1d0ced946895f142f72a99a3c3b14a9eb18b5ce
qiaoxia.gao <qiaoxia.gao@wmeimob.com>
2019-08-21 14:20:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import login from './image/login.png'
+import logo from './image/logo_2.png'
 import './App.css';
+// import 'font-awesome/less/font-awesome.less';
+// import 'react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Login">
+      <img src={logo} className="logo_Image"></img>
+      <div className="Login_Content">
+        <div clascsName="flex Login_header">
+          <h3 className="Login_header_tip">欢迎登录</h3>智橙移动优化后台
+        </div>
+
+        <div className="Login_Center">
+          <div className="flex Login_Center_input">
+            {/* <i className="fa fa-camera-retro"></i> */}
+            <input type="text" placeholder="请输入用户名" className="inputName" />
+          </div>
+          <div className="flex Login_Center_input">
+            <img src="" className="pic_image"></img>
+            <input type="password" placeholder="请输入密码" className="inputName" />
+          </div>
+          <div className="flex Login_Center_input">
+            <input type="number" placeholder="请输入验证码" className="inputName" />
+            <button className="code_Name">获取验证码</button>
+          </div>
+          <button className="button_Name">登录</button>
+        </div>
+
+        {/* <img src={login} /> */}
+      </div>
     </div>
   );
 }
